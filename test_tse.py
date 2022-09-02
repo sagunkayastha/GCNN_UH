b91516e86abb771cbbf387a0eed0c0c5fe937e2e
sagunkayastha <sgn.kayastha@gmail.com>
2022-09-02 15:46:34 -0500

    split

---
+++ test_tse.py
@@ -0,0 +1,12 @@
+import os
+
+
+path1= "GCNN_UH"
+path2 = "example"
+path3 = "places2"
+
+total_path = os.path.join(path1, path2, path3)
+files = os.listdir(total_path)
+
+for file in os.path.join(path1, path2, path3, files:
+    
